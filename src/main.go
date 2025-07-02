b9e58d1866141ba31eab6f81044a9a64de94b837
Wathsala Vishwajith <wathsala-vishwajith@github.com>
2025-07-02 20:39:06 +0530

    add preproceessing part

--- src/main.go
+++ src/main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "go-data-preprocessor/config"
-    "go-data-preprocessor/processor"
-    "go-data-preprocessor/writer"
+	"analytics-in-go/src/config"
+	"analytics-in-go/src/processor"
+	"analytics-in-go/src/writer"
 )
 
 func main() {
-    cfg, err := config.LoadConfig("config/country_revenue.yaml")
-    if err != nil {
-        log.Fatalf("Failed to load config: %v", err)
-    }
+	cfg, err := config.LoadConfig("config/country_level_revenue.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 
-    rows, err := processor.LoadCSV(cfg.InputFile)
-    if err != nil {
-        log.Fatalf("Failed to load CSV: %v", err)
-    }
+	rows, err := processor.LoadCSV(cfg.InputCSV)
+	if err != nil {
+		log.Fatalf("Failed to load CSV: %v", err)
+	}
 
-    aggRows, err := processor.Aggregate(rows, cfg)
-    if err != nil {
-        log.Fatalf("Failed to aggregate: %v", err)
-    }
+	aggRows, err := processor.Aggregate(rows, cfg)
+	if err != nil {
+		log.Fatalf("Failed to aggregate: %v", err)
+	}
 
-    err = writer.WriteParquet(cfg.OutputFile, aggRows)
-    if err != nil {
-        log.Fatalf("Failed to write parquet: %v", err)
-    }
+	err = writer.WriteParquet(cfg.OutputParquet, aggRows)
+	if err != nil {
+		log.Fatalf("Failed to write parquet: %v", err)
+	}
 
-    fmt.Println("✅ Preprocessing complete. Output written to:", cfg.OutputFile)
+	fmt.Println("✅ Preprocessing complete. Output written to:", cfg.OutputParquet)
 }
-
